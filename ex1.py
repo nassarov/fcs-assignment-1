e8001301845d57338487ffee8274ec010ef22fe4
Ali Nassar <aliinassar8@gmail.com>
2025-02-14 01:48:46 +0200

    Declaring age variable that will have the value of user's input

---
+++ ex1.py
@@ -0,0 +1,12 @@
+#  1) Exercise 1: Movie Ticket Price Calculator Write a Python program that asks the user for their age and determines the price of a movie ticket based on the following rules:
+# - If the user is , the ticket is
+# - If the user is , the ticket costs .
+# - If the user is , the ticket costs .
+# - If the user is , the ticket costs .
+# Example:
+# Enter your age: 8
+# The ticket price is $5.
+
+age = int(input("Enter your age: "))
+
+
