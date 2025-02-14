3349227204604e1a1ffef19873a37ee92d28280d
Ali Nassar <aliinassar8@gmail.com>
2025-02-14 02:17:49 +0200

    Checking on user input if both username and password and print corresponding message

--- ex3.py
+++ ex3.py
@@ -15,3 +15,8 @@
 
 username = input("Enter username: ")
 password = input("Enter password: ")
+
+if username == "admin" and password == "1234":
+    print("Access granted")
+else:
+     print("Access denied")
