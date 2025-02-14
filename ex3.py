6aee8812de152c4f7126fd14bc02a643f662ab3b
Ali Nassar <aliinassar8@gmail.com>
2025-02-14 02:16:51 +0200

    Declaring username and password as input from user

---
+++ ex3.py
@@ -0,0 +1,17 @@
+# 3) Simple Login System
+
+# Write a Python program that asks the user for a username and password.
+# If the username is "admin" and the password is "1234", print "Access granted".
+# Otherwise, print "Access denied".
+# Example:
+# Enter username: admin 
+# Enter password: 1234 
+# Access granted
+
+# Enter username: user 
+# Enter password: pass 
+# Access denied
+
+
+username = input("Enter username: ")
+password = input("Enter password: ")
